5e66c4c9f2192f997929a9d9127847d78aa40eb0
Luiz Mugnaini <luizmugnaini@gmail.com>
2021-08-31 10:17:43 -0300

    First commit of dekker

---
+++ dekker/dekker.py
@@ -0,0 +1,20 @@
+################################################################################
+# Authors: Luiz Gustavo Mugnaini Anselmo (nUSP: 11809746)
+#          Victor Manuel Saliba (nUSP: )
+#          Luan Marc (nUSP: )
+#
+# Dekker method for finding roots of a given function.
+################################################################################
+
+def dekker(f, a, b):
+    # Dekker method
+    if f(a) * f(b) >= 0:
+        raise Exception('The function must change sign in [a, b]')
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
